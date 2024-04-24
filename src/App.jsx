69024696493f60c53c6d89360b01e73633a4bb38
Miclaine Emtman <clainie@gmail.com>
2024-04-24 16:12:42 -0700

    Add scores

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,39 @@ import { useEffect } from 'react'
 import fillPokemonArray from './data.js'
 import './App.css'
 import Grid from './Grid.jsx'
+import Score from './Score.jsx'
 
 function App() {
   console.log("reloading App");
   const allPokemonArray = fillPokemonArray();
   const displayedPokemonArray = shuffleArray(allPokemonArray);
+  console.log(displayedPokemonArray);
   const [clicked, setClickedPokemon] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
   useEffect(() => {
     shuffleArray(allPokemonArray);
-  }, [clicked]);
+  }, [clicked, allPokemonArray]);
 
   const handleClick = (e) => {
     let clickedPokemon = allPokemonArray.find((element) => element.id === e);
-    clickedPokemon.clicked = true;
-    console.log(clickedPokemon);
-    const newClick = [e, ...clicked];
-    console.log(newClick);
-    setClickedPokemon(newClick);
+    if (clickedPokemon.clicked === false) {
+      clickedPokemon.clicked = true;
+      const newClick = [e, ...clicked];
+      setClickedPokemon(newClick);
+      setCurrentScore(currentScore + 1);
+
+    } else {
+      console.log("You already clicked that pokemon!");
+      if (currentScore > highScore) {
+        setHighScore(currentScore);
+      }
+      setCurrentScore(0);
+      setClickedPokemon([]);
+      allPokemonArray.forEach((pokemon) => pokemon.clicked = false);
+    }
   }
-  // Fisher-Yates Sorting Algorithm
   function shuffleArray(array) {
     array.sort(() => 0.5 - Math.random());
     let shuffled = array.slice(0, 11);
@@ -31,6 +45,7 @@ function App() {
     <>
       <h1>Memory Card Game</h1>
       <h2>Click each card only once!</h2>
+      <Score currentScore={currentScore} highScore={highScore} />
       <Grid array={displayedPokemonArray} handleClick={handleClick} />
 
     </>
---
+++ src/Score.jsx
@@ -0,0 +1,10 @@
+export default function Score({ currentScore, highScore }) {
+    return (
+        <div>
+            <h4>Current Score: {currentScore}</h4>
+            <h4>High Score: {highScore}</h4>
+
+        </div>
+
+    )
+}
